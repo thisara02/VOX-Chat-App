3775e16e639e6de039f4407137b053e299414347
Thisara Madusanka <137640738+thisara02@users.noreply.github.com>
2024-04-10 23:57:25 +0530

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:887130118652:web:2f4f68e54937fc28f13a79";
+  static String apiKey = "AIzaSyBYWy4vWdz-Vb2kHfQvxwvLIQiNpuKbrTc";
+  static String messagingSenderId = "887130118652";
+  static String projectId = "chatappflutter-f7c58";
+  final primaryColor = Color.fromARGB(202, 2, 38, 85);
+}
